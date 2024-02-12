7c91def9d228e2f160a94be79e1576bbfd60d065
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-12 12:02:41 +0100

    Inserito in html form e memorizzato in js

---
+++ jsnack1/js/script.js
@@ -0,0 +1,14 @@
+// Snack 1:
+// Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+const buttonElement = document.querySelector("button").addEventListener("click", function(){
+
+    let userChoice = document.querySelector("#userChoice").value;
+    console.log(userChoice)
+
+
+
+
+
+
+});
