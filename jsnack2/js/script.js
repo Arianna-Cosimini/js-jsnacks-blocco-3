afa6032cee7a71728cb3933be3d5bf39d34b0b50
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-12 16:51:49 +0100

    Inizio snack 3

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -3,27 +3,68 @@
 //  (date per scontato che abbiano lo stesso numero di elementi) 
 //  prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+// const buttonElement = document.querySelector("button");
+// buttonElement.addEventListener("click", function () {
 
-// creo array nomi
+//     // creo array nomi
+//     const userName = document.querySelector("#userName").value;
+//     const nameList = [];
+//     // ["Arianna","Nicolò","Emenuele"];
+
+//     // creo array numeri
+//     const userNumber = document.querySelector("#userNumber").value;
+//     const numberList = [];
+
+
+//     for (let i = 0; i < nameList.length; i++) {
+//         nameList.push(userName[i])
+//         console.log(nameList)
+//     }
+
+//     for (let i = 0; i < numberList.length; i++) {
+//         numberList.push(userNumber[i])
+//         console.log(numberList)
+//     }
+
+
+
+
+//    
+
+
+
+
+// })
+
 const nameList = ["Arianna","Nicolò","Emenuele"];
-console.log(nameList)
 
-// creo array numeri
-const numberList = ["27","33","5"]
-console.log(numberList)
+const numberList = ["27", "33","5"]
 
-
-function betweenArray (nameList, numberList){
+function betweenArray(nameList, numberList) {
     const numberPlusNameList = [];
-    for (let i = 0  ; i <  numberList.length; i++ ){
-        numberPlusNameList.push(nameList[i])
-        numberPlusNameList.push(numberList[i])
-        console.log(numberPlusNameList)
+
+
+    if (nameList.length < numberList.length) {
+        nameList.push(userName)
+    } else if (numberList.length < nameList.length) {
+        numberList.push(userNumber)
+    } else if (nameList.length == numberList.length) {
+
+        for (let i = 0; i < numberList.length; i++) {
+
+            numberPlusNameList.push(nameList[i])
+            numberPlusNameList.push(numberList[i])
+
+            console.log(numberPlusNameList)
+        }
     }
+
+
 
     return numberPlusNameList;
 }
 
+    let result = betweenArray(nameList, numberList);
+    console.log(result)
 
-let result = betweenArray(nameList,numberList);
-console.log(result)
+
