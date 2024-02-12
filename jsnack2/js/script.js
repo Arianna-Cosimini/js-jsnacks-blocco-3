4c5db2599d5e734616350e6c32050c348a9caf1d
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-12 14:03:49 +0100

    creata funzione per unire gli array

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -4,8 +4,22 @@
 //  prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
+// creo array nomi
+const nameList = ["Arianna","Nicolò","Emenuele"];
+console.log(nameList)
 
-const nameList = ["Arianna", "Nicolò", "Emenuele"];
-console.dir(nameList)
+// creo array numeri
 const numberList = ["27","33","5"]
 console.log(numberList)
+
+
+function betweenArray (nameList, numberList){
+    const numberPlusNameList = [];
+    for (let i = 0  ; i <  nameList.length; i++ ){
+        
+    }
+}
+
+betweenArray (nameList,numberList);
+// console.log(betweenArray)
+// console.log(nameList.length[i] + 1)
