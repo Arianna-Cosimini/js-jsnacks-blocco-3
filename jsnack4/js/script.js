462c5dd64f3e362c5d45f87e18a84116bec5e4b6
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-12 18:00:53 +0100

    Creato prompt, creato array e ciclo for per iterare le lettere della parola inserita dall'utente

---
+++ jsnack4/js/script.js
@@ -0,0 +1,13 @@
+// Snack 4 (bonus):
+// Scrivi una funzione  che accetti una stringa come parametro
+//  e restituisca la stringa con i caratteri ordinati alfabeticamente
+// Es: riordina("webmaster") :freccia_destra: "abeemrstw"
+
+
+let userChoice = prompt("inserisci una parola");
+const result = [];
+for(let i = 0; i < userChoice.length; i++){
+
+    result.push(userChoice[i])
+    console.log(result)
+}
